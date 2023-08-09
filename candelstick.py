8e2bd94e82dcb9189bfb65100c0c5b032edd6a19
Aayushi Patel <126417125+aayushipatel135@users.noreply.github.com>
2023-08-09 12:57:57 +0530

    Update candelstick.py

--- candelstick.py
+++ candelstick.py
@@ -100,7 +100,7 @@ def update_graph_scatter(n):
         )
         last = last + 1
         return {'data': [candle,],
-                'layout' : go.Layout(xaxis_rangeslider_visible=False,)}
+                'layout' : go.Layout(xaxis_rangeslider_visible=True,)}
     else : 
         X.append(X[-1]+1)
         Y.append(Y[-1]+Y[-1] * random.uniform(-0.1,0.1))
